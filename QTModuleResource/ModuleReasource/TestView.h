1ee605dff6197b6fadc1e218b376e4610ece9807
zhoupengkai <15556532221@163.com>
2018-11-29 09:31:50 +0800

    xib view

---
+++ QTModuleResource/ModuleReasource/TestView.h
@@ -0,0 +1,17 @@
+//
+//  TestView.h
+//  QTModuleResource
+//
+//  Created by ☺strum☺ on 2018/11/29.
+//  Copyright © 2018年 l. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ QTModuleResource/ModuleReasource/TestView.m
@@ -0,0 +1,21 @@
+//
+//  TestView.m
+//  QTModuleResource
+//
+//  Created by ☺strum☺ on 2018/11/29.
+//  Copyright © 2018年 l. All rights reserved.
+//
+
+#import "TestView.h"
+
+@implementation TestView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
